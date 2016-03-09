5b039d1190f1d6b03b42f3b9fe968894a4f3e0db
User2 <User2>
2016-03-09 18:43:08 +0600

    Initial commit

---
+++ app/src/main/java/bd/com/mcc/imagegallery/DataModel.java
@@ -0,0 +1,38 @@
+package bd.com.mcc.imagegallery;
+
+/**
+ * Created by User1 on 3/9/2016.
+ */
+public class DataModel {
+
+
+    String name;
+    String version;
+    int id_;
+    int image;
+
+    public DataModel(String name, String version, int id_, int image) {
+        this.name = name;
+        this.version = version;
+        this.id_ = id_;
+        this.image=image;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getVersion() {
+        return version;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public int getId() {
+        return id_;
+    }
+}
---
+++ app/src/main/java/bd/com/mcc/imagegallery/MyData.java
@@ -0,0 +1,16 @@
+package bd.com.mcc.imagegallery;
+
+/**
+ * Created by User1 on 3/9/2016.
+ */
+public class MyData {
+
+    static String[] nameArray = {"Cupcake", "Donut", "Eclair", "Froyo", "Gingerbread", "Honeycomb", "Ice Cream Sandwich","JellyBean", "Kitkat", "Lollipop", "Marshmallow"};
+    static String[] versionArray = {"1.5", "1.6", "2.0-2.1", "2.2-2.2.3", "2.3-2.3.7", "3.0-3.2.6", "4.0-4.0.4", "4.1-4.3.1", "4.4-4.4.4", "5.0-5.1.1","6.0-6.0.1"};
+
+    static Integer[] drawableArray = {R.drawable.chrysanthemum, R.drawable.desert, R.drawable.hydrangeas,
+            R.drawable.jellyfish, R.drawable.koala, R.drawable.lighthouse, R.drawable.penguins,
+            R.drawable.tulips, R.drawable.koala, R.drawable.jellyfish,R.drawable.desert};
+
+    static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+}
